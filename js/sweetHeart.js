726929921c2eff60b8de2f54133c98775398783a
陈月 <chenyue@areozhonghuan.com>
2019-11-12 17:31:38 +0800

    美化小心心

--- js/sweetHeart.js
+++ js/sweetHeart.js
@@ -11,6 +11,7 @@
         }
     })();
     init();
+    setInterval(createHeart, 800);
     function init() {
         css(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: absolute;}.heart:after{top: -5px;}.heart:before{left: -5px;}");
         attachEvent();
@@ -24,8 +25,8 @@
                 continue;
             }
             hearts[i].y--;
-            hearts[i].scale += 0.004;
-            hearts[i].alpha -= 0.013;
+            hearts[i].scale += 0.005;
+            hearts[i].alpha -= 0.005;
             hearts[i].el.style.cssText = "left:" + hearts[i].x + "px;top:" + hearts[i].y + "px;opacity:" + hearts[i].alpha + ";transform:scale(" + hearts[i].scale + "," + hearts[i].scale + ") rotate(45deg);background:" + hearts[i].color;
         }
         requestAnimationFrame(gameloop);
@@ -40,10 +41,22 @@
     function createHeart(event) {
         var d = document.createElement("div");
         d.className = "heart";
+        var x, y;
+        if (event) {
+            x = event.clientX - 5;
+            y = event.clientY - 5;
+        } else {
+            var maxX = window.screen.availWidth,
+                maxY = window.screen.availHeight;
+
+            x = Math.floor(Math.random() * (maxX + 1));
+            y = Math.floor(Math.random() * (maxY + 1));
+        }
+
         hearts.push({
             el : d,
-            x : event.clientX - 5,
-            y : event.clientY - 5,
+            x : x,
+            y : y,
             scale : 1,
             alpha : 1,
             color : randomColor()
@@ -63,4 +76,4 @@
     function randomColor() {
         return "rgb(" + (~~(Math.random()*255)) + "," + (~~(Math.random()*255)) + "," + (~~(Math.random()*255)) + ")";
     }
-})(window,document);
+})(window,document);    
